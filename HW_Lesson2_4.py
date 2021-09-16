48a74bb22b28efc546c294408e77394b2b495236
Hryhorii <grigoriy29091999@gmail.com>
2021-09-16 16:03:50 +0300

    .

--- HW_Lesson2_4.py
+++ HW_Lesson2_4.py
@@ -1,15 +1,27 @@
 d = {'title': "Gunpowder", 'price': 979, 'ingredients': ['nitrate', 'charcoal', 'sulfur']}
 
+
 d['description'] = 'Gunpowder, also known as black powder to distinguish it from modern smokeless powder, is the earliest known chemical explosive'
 print('4.1 ' + d['title'], d['price'], d['ingredients'], d['description'])
+
 
 d.update({'price': d['price'] + 100})
 print('4.2 ' + d['title'], d['price'], d['ingredients'], d['description'])
 
+
 d['ingredients'].append('dry air')
 print('4.3 ' + d['title'], d['price'], d['ingredients'], d['description'])
 
-print(f'4.4 There is(are) {len(d["ingredients"])} element(s)')
+
+if len(d["ingredients"]) == 0:
+    print(f'4.4 There is {len(d["ingredients"])} ingredient')
+
+if len(d["ingredients"]) == 1:
+    print(f'4.4 There is {len(d["ingredients"])} ingredient')
+
+else:
+    print(f'4.4 There are {len(d["ingredients"])} ingredients')
+
 
 d.pop('description')
 print('4.5 ' + d['title'], d['price'], d['ingredients'])
